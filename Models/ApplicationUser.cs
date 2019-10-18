4785b59971b5f6344a7e0f4c2cc13ce0985fb249
Andiswa Ndlovu <Andiswa Ndlovu@DESKTOP-1JMOLO6>
2019-10-18 13:57:20 +0200

    styling login page

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DispatchMe.Web.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int PersonId { get; set; }
+        public int UserTypeId { get; set; }
+        public virtual UserType UserType { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DispatchMe.Web.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public int ContactTypeId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string TellNumber { get; set; }
+        public string WorkNumber { get; set; }
+        public string PersonalEmail { get; set; }
+        public string WorkEmail { get; set; }
+        public virtual ContactType ContactType { get; set; }
+        public  virtual Person Person  { get; set; }
+    }
+}
